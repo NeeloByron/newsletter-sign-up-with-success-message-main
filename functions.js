004403717f2db65f3f181a4b58b6cf42e893d76b
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-06-07 21:20:31 +0200

    Error message & form submission

--- functions.js
+++ functions.js
@@ -11,3 +11,33 @@ function isValidEmail (email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
+
+//show error
+function showError() {
+    emailInput.classList.add('error')
+    errorMessage.classList.add('show');
+}
+
+//hide error
+function hideError() {
+    emailInput.classList.remove('error');
+    errorMessage.classList.remove('show');
+}
+
+//form submission
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const email = emailInput.value.trim();
+
+    if (!isValidEmail(email)) {
+        showError();
+    }
+    else {
+        hideError();
+        userEmailSpan.textContent = email;
+        signupContainer.style.display = 'none';
+        succesContainer.style.display = 'flex';
+    }
+
+}); 
