ac69773d891eed32ba7d1d077c50c8a834222eda
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-06-08 22:54:14 +0200

    changed email setting

--- functions.js
+++ functions.js
@@ -9,7 +9,7 @@ const userEmailSpan = document.getElementById('userEmail');
 
 // Email validation function
 function isValidEmail (email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
 
